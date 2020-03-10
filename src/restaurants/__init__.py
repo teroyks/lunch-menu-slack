170aea9a97bda6960124f223b4ca2a5d10498cf2
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2020-03-10 17:48:59 +0200

    ✨ Create restaurant modules list dynamically
    Includes all Python files in the restaurants directory, except for _*.py

--- src/restaurants/__init__.py
+++ src/restaurants/__init__.py
@@ -1,2 +1,21 @@
 """Include all modules in the directory"""
-__all__ = ['mauno', 'herkkupiste']
+
+from os import path, scandir, DirEntry
+
+
+def _is_restaurant_module(entry: DirEntry) -> bool:
+    """Checks if entry is a Python file, ignores if name starts with underscore."""
+    return all([
+        entry.is_file(),
+        entry.name.endswith('.py'),  # type: ignore
+        not entry.name.startswith('_')  # type: ignore
+    ])
+
+
+def _module_name(filename: str) -> str:
+    return path.splitext(filename)[0]
+
+
+with scandir(path.dirname(__file__)) as dir_it:
+    __all__ = [_module_name(entry.name)
+               for entry in dir_it if _is_restaurant_module(entry)]
