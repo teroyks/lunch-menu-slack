942670ba8077c05ce7c4b16c14e46b7192450eca
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2020-03-10 13:47:58 +0200

    ✅ Add Lounaat.info parsing test
    Parses real data fetched from lounaat.info and saved into a fixture.

--- tests/unit/test_lounaat_info.py
+++ tests/unit/test_lounaat_info.py
@@ -1,15 +1,17 @@
 """Tests the lounaat_info helper function with a mock API and Herkkupiste data."""
 
 from datetime import date
+import os
+import pickle
 import responses
 
 from helpers import MenuMsg as Msg
 from lounaat_info import LOUNAAT_URL, fetch_from_lounaat
 
-URL = 'foo'
+URL = 'herkkupiste/turku'
 FULL_URL = f'{LOUNAAT_URL}/{URL}'
 
-day = date(2020, 3, 10)
+day = date(2020, 3, 11)
 
 
 @responses.activate
@@ -29,3 +31,16 @@ def test_invalid_content():
 
     res = fetch_from_lounaat(URL, day)
     assert res == Msg.MENU_NOT_FOUND, 'Should not be able to parse the menu.'
+
+
+def test_parse_herkkupiste_data():
+    """Parse real restaurant data."""
+    fixture_dir = os.path.dirname(os.path.realpath(__file__)) + '/fixtures'
+
+    with open(fixture_dir + '/herkkupiste.response', 'rb') as fixture:
+        cached = pickle.load(fixture)
+        responses.add(responses.GET, FULL_URL,
+                      status=cached.status_code, body=cached.text)
+
+    menu = fetch_from_lounaat(URL, day)
+    assert 'Paahdettua lohta' in menu, 'Should have parsed the menu for given day.'
