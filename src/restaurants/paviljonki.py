c18e44e56e16e59921cb12352b70dfa8899b9b65
Tero Ykspetäjä <tero@tero.dev>
2020-03-12 13:15:06 +0200

    💄 Format Paviljonki output
    Add emphasis on menu subheadings.

--- src/restaurants/paviljonki.py
+++ src/restaurants/paviljonki.py
@@ -1,6 +1,7 @@
 """Kupittaan Paviljonki menu"""
 
 from datetime import date
+import re
 
 from lounaat_info import fetch_from_lounaat
 from model.restaurant import Restaurant
@@ -9,12 +10,17 @@ NAME = 'Kupittaan Paviljonki'
 URL = 'https://www.kupittaanpaviljonki.fi/'
 
 
+def _format(menu: str) -> str:
+    return re.sub(r'(Noutopöytä|Pöytään.*)\n', r'*\1*\n', menu)
+
+
 def fetch() -> Restaurant:
     """Fetches the menu."""
     return Restaurant(
         name=NAME,
         url=URL,
-        menu=fetch_from_lounaat('kupittaan-paviljonki/turku', date.today())
+        menu=_format(fetch_from_lounaat(
+            'kupittaan-paviljonki/turku', date.today()))
     )
 
 
